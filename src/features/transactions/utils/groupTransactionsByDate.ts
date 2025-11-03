a313ebcb787919a2b42bd1a7a1d94e9192a94aa3
unknown <ami145388@gmail.com>
2025-11-03 23:42:55 +0500

    refactor(transactions): moved filtering and sorting transaction login into hook

--- src/features/transactions/utils/groupTransactionsByDate.ts
+++ src/features/transactions/utils/groupTransactionsByDate.ts
@@ -4,23 +4,24 @@ export function groupTransactionsByDate(
   transactions: ITransaction[],
   isReversed = false,
 ) {
-  const grouped = Object.entries(
-    transactions.reduce((acc: Record<string, ITransaction[]>, tx) => {
-      const date = new Date(tx.date).toLocaleDateString('en-US', {
-        year: 'numeric',
-        month: 'short',
-        day: 'numeric',
-      });
-      if (!acc[date]) acc[date] = [];
-      acc[date].push(tx);
-      return acc;
-    }, {}),
-  ).sort(
-    ([a], [b]) =>
-      isReversed
-        ? new Date(a).getTime() - new Date(b).getTime() // ascending
-        : new Date(b).getTime() - new Date(a).getTime(), // descending
+  const map = transactions.reduce((acc: Record<string, ITransaction[]>, tx) => {
+    const date = new Date(tx.date).toLocaleDateString('en-US', {
+      year: 'numeric',
+      month: 'short',
+      day: 'numeric',
+    });
+    if (!acc[date]) acc[date] = [];
+    acc[date].push(tx);
+    return acc;
+  }, {});
+
+  const entries = Object.entries(map);
+
+  entries.sort(([a], [b]) =>
+    isReversed
+      ? new Date(a).getTime() - new Date(b).getTime()
+      : new Date(b).getTime() - new Date(a).getTime(),
   );
 
-  return grouped;
+  return entries;
 }
---
+++ src/features/transactions/utils/useFilteredSortedTransactions.ts
@@ -0,0 +1,21 @@
+import { useMemo } from 'react';
+import type {
+  ITransaction,
+  ITransactionFiltersState,
+  TTransactionsSortState,
+} from '../../../types/transaction.types';
+import { filterTransactions } from './filterTransactions';
+import { sortTransactions } from './sortTransactions';
+
+export const useFilteredSortedTransactions = (
+  transactions: ITransaction[],
+  filters: ITransactionFiltersState,
+  sortBy: TTransactionsSortState,
+  isReversed: boolean,
+) => {
+  return useMemo(() => {
+    const filtered = filterTransactions(transactions, filters);
+    const sorted = sortTransactions(filtered, sortBy);
+    return isReversed ? [...sorted].reverse() : sorted;
+  }, [transactions, filters, sortBy, isReversed]);
+};
